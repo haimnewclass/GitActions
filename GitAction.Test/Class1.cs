0ab3be5b33fa163b60d10434ab54d5ffd7653dc7
haimnewclass <haim@newclass.co.il>
2023-04-18 16:01:27 +0300

    test

--- GitAction.Test/Class1.cs
+++ GitAction.Test/Class1.cs
@@ -1,7 +1,14 @@
-﻿namespace GitAction.Test
+﻿using NUnit;
+using NUnit.Framework;
+
+namespace GitAction.Test
 {
     public class Class1
     {
-
+        [Test]
+        public void Run()
+        {
+            Assert.AreEqual(1, 1);
+        }
     }
 }
