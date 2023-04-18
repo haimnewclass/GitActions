94708b7b5ae231b0a1fec7e77a2505e2abb34d1b
haimnewclass <haim@newclass.co.il>
2023-04-18 16:10:01 +0300

    test5

---
+++ GitAction.Test2/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
+
+namespace GitAction.Test2
+{
+    public class Class1
+    {
+        [Test]
+        public void Run()
+        {
+            Assert.AreEqual(1, 0, "Not Equal");
+        }
+    }
+}
